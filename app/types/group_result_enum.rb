871d491963da0ca7c4a546c5cdb903b0cfac46ae
khor <khor@anyperk.com>
2015-09-27 14:22:34 +0900

    Remove app/types that we wrongly added when doing 'mv'

--- app/types/group_result_enum.rb
+++
@@ -1,7 +0,0 @@
-GroupResultEnum = GraphQL::EnumType.define do
-  name "GroupResultEnum"
-  description "Group results belong to either one of these"
-  Result::GROUP_TYPES.each do |numeric, desc|
-    value(desc, "#{desc}", value: numeric)
-  end
-end
--- app/types/simple_type.rb
+++
@@ -1,7 +0,0 @@
-SimpleType = GraphQL::ObjectType.define do
-  name "Simple"
-  description "Simple"
-  interfaces [NodeIdentification.interface]
-  global_id_field :id
-  field :name, !types.String, "Simple name"
-end
